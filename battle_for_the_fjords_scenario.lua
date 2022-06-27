c030d457dced22cb17883f434fe5e91328bfebb2
Rowey <row.josh02@gmail.com>
2022-06-28 00:02:20 +0100

    Add Map Files
    Adding all the files from the map folder

---
+++ battle_for_the_fjords_scenario.lua
@@ -0,0 +1,28 @@
+version = 3 -- Lua Version. Dont touch this
+ScenarioInfo = {
+    name = "Battle for the Fjords",
+    description = "As spring arrives, the ice and snow that keeps the Sirenukall river frozen begins to melt, and the river starts to flow again.",
+    preview = '',
+    map_version = 2,
+    type = 'campaign_coop',
+    starts = true,
+    size = {1024, 1024},
+    reclaim = {104409, 652640},
+    map = '/maps/battle_for_the_fjords/battle_for_the_fjords.scmap',
+    save = '/maps/battle_for_the_fjords/battle_for_the_fjords_save.lua',
+    script = '/maps/battle_for_the_fjords/battle_for_the_fjords_script.lua',
+    norushradius = 40,
+    Configurations = {
+        ['standard'] = {
+            teams = {
+                {
+                    name = 'FFA',
+                    armies = {'ARMY_1', 'ARMY_2', 'ARMY_3', 'ARMY_4', 'ARMY_5', 'ARMY_6', 'ARMY_7', 'ARMY_8'}
+                },
+            },
+            customprops = {
+                ['ExtraArmies'] = STRING( 'ARMY_17 NEUTRAL_CIVILIAN' ),
+            },
+        },
+    },
+}
